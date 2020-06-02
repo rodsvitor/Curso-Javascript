2ff6ccf33a179d600347d01e8c1551edff91ba14
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-02 19:20:41 -0300

    Aprendendo sobre axios

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-function minhaPromise() {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', 'https://api.github.com/users/rodsvitor');
-    xhr.send(null);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) { // resposta voltou
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject('Erro na requisição!');
-        }
-      }
-    }
-  });
-}
-
-minhaPromise()
+//deve-se adicionar um src de javascript para usar o axios.
+axios.get('https://api.github.com/users/rodsvitor')
   .then(console.log)
   .catch(console.warn);
-
-// var resultado = minhaPromise();
-// console.log(resultado); //Mostratá um resultado pendente, pois não
-//                         //se esperou o resultado completo do servidor
-
